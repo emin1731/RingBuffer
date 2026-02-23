a194991c78c9322ab7d3c45fc614b80e1ba41c4a
emin1731 <emin1731@icloud.com>
2026-02-23 21:01:14 +0400

    ref: update the attribute naming of RingBuffer, refactor the read and write methods

--- RingBuffer.java
+++ RingBuffer.java
@@ -1,64 +1,47 @@
-import java.util.ArrayList;
-import java.util.List;
- 
 public class RingBuffer {
-  List<Integer> buffer;
-  int w;
-  int r;
-  int size;
-  int count;
+  private int[] buffer;
+  private int size;
+  private int writeIndex;
+  private int writeCount;
  
   public RingBuffer(int size) {
+    this.buffer = new int[size];
     this.size = size;
-    this.w = 0;
-    this.r = 0;
-    this.count = 0;
-    this.buffer = new ArrayList<>(size);
+    this.writeIndex = 0;
+    this.writeCount = 0;
  
     for (int i = 0; i < size; i++) {
-      buffer.add(0);
+      buffer[i] = 0;
     }
   }
  
-  public void write(int x) {
-    buffer.set(w, x);
-    w++;
-    if (w == size) {
-      w = 0;
+  public void write(int value) {
+    buffer[writeIndex] = value;
+    writeIndex++;
+
+    if (writeIndex == size) {
+      writeIndex = 0;
     }
  
-    if (count == size) {
-      r++;
-      if (r == size) {
-        r = 0;
-      }
+    if (writeCount == size) {
+      writeCount = size;
     } else {
-      count++;
+      writeCount++;
     }
   }
- 
-  public void read() {
-    if (count == 0) {
-      System.out.println("Buffer is empty, nothing to read");
-      return; // case we read all the messages
+
+  public int read(int index) {
+    if (index < 0 || index >= writeCount) {
+      throw new IndexOutOfBoundsException("Index: " + index + ", Write Count: " + writeCount);
     }
- 
-    int value = buffer.get(r);
-    r++;
-    if (r == size) {
-      r = 0;
-    }
- 
-    count--;
- 
-    System.out.println("You read this message: " + value);
+    return buffer[index];
   }
  
   public void print() {
-    System.out.println("This is our buffer now: " + buffer);
-    System.out.println("w: " + w);
-    System.out.println("r: " + r);
-    System.out.println("count: " + count);
+    System.out.println("This is our buffer now: " + java.util.Arrays.toString(buffer));
+    System.out.println("w: " + writeIndex);
+
+    System.out.println("Write count: " + writeCount);
   }
  
 }
